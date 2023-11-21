06a904f41eb6a443de7f1076937fcb03e3296352
Samantha Johnson <samantha.nisus@gmail.com>
2023-11-20 22:24:48 -0600

    Components Part 1

--- App.js
+++ App.js
@@ -1,20 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Main from "./screens/MainComponent";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Welcome Samantha!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return <Main />;
+};
