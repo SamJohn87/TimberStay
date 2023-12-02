155cc107c2929a595287c612bf2473e572dd0242
samantha_nisus_johnson <128253624+SamJohn87@users.noreply.github.com>
2023-12-01 21:34:56 -0600

    week 2 workshop Part 1

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -2,34 +2,40 @@ import { StyleSheet, Text, View } from "react-native";
 import { Card, Icon } from "react-native-elements";
 import { baseUrl } from "../../shared/baseUrl";
 
-const RenderCampsite = ({ campsite, isFavorite, markFavorite }) => {
+const RenderCampsite = ({ campsite, isFavorite, markFavorite, onShowModal }) => {
     if (campsite) {
         return (
             <Card containerStyle={styles.cardContainer}>
-                <Card.Image source={{ uri: `${baseUrl}${campsite.image}`}}>
+                <Card.Image source={{ uri: `${baseUrl}${campsite.image}` }}>
                     <View style={{ justifyContent: 'center', flex: 1 }}>
-                        <Text style={{
-                            color: 'white',
-                            textaligne: 'center',
-                            fontSize: 20
-                        }}>
+                        <Text style={styles.cardText}>
                             {campsite.name}
                         </Text>
                     </View>
                 </Card.Image>
                 <Text style={{ margin: 20 }}>{campsite.description}</Text>
-                <Icon
-                    name={isFavorite ? 'heart' : 'heart-o'}
-                    type='font-awesome'
-                    color='#f50'
-                    raised
-                    reverse
-                    onPress={
-                        () => isFavorite
-                            ? console.log('Already set as a favorite')
-                            : markFavorite()
-                    }
-                ></Icon>
+                <View style={styles.cardRow}>
+                    <Icon
+                        name={isFavorite ? 'heart' : 'heart-o'}
+                        type='font-awesome'
+                        color='#f50'
+                        raised
+                        reverse
+                        onPress={
+                            () => isFavorite
+                                ? console.log('Already set as a favorite')
+                                : markFavorite()
+                        }
+                    />
+                    <Icon
+                        name='pencil'
+                        type='font-awesome'
+                        color='#5637DD'
+                        raised
+                        reverse
+                        onPress={onShowModal}
+                    />
+                </View>
             </Card>
         );
     }
@@ -42,6 +48,21 @@ const styles = StyleSheet.create({
         padding: 0,
         margin: 0,
         marginBottom: 20
+    },
+    cardRow: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        flexDirection: 'row',
+        margin: 20
+    },
+    cardText: {
+        textShadowColor: 'rgba(0,0,0,1',
+        textShadowOffset: { width: -1, height: 1 },
+        textShadowRadius: 20,
+        textAlign: 'center',
+        color: 'white',
+        fontSize: 20
     }
 })
 
