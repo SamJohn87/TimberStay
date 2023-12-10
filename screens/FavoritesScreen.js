68d4dad5dc3ec54becc056f4be77e600aaf0b553
samantha_nisus_johnson <128253624+SamJohn87@users.noreply.github.com>
2023-12-09 20:36:27 -0600

    Animatable

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -6,6 +6,7 @@ import { Alert } from "react-native";
 import { toggleFavorite } from "../features/favorites/favoritesSlice";
 import { baseUrl } from '../shared/baseUrl';
 import Loading from '../components/LoadingComponent';
+import * as Animatable from 'react-native-animatable';
 
 const FavoritesScreen = ({ navigation }) => {
     const { campsitesArray, isLoading, errMess } = useSelector((state) => state.campsites);
@@ -29,7 +30,7 @@ const FavoritesScreen = ({ navigation }) => {
                                     onPress: () => dispatch(toggleFavorite(campsite.id))
                                 }
                             ],
-                            {cancelable: false}
+                            { cancelable: false }
                         )}>
                         <Text style={styles.deleteText}>Delete</Text>
                     </TouchableOpacity>
@@ -65,11 +66,16 @@ const FavoritesScreen = ({ navigation }) => {
     }
 
     return (
-        <FlatList
-            data={campsitesArray.filter((campsite) => favorites.includes(campsite.id))}
-            renderItem={renderFavoriteItem}
-            keyExtractor={(item) => item.id.toString()}
-        />
+        <Animatable.View
+            animation='fadeInRightBig'
+            duration={2000}
+        >
+            <FlatList
+                data={campsitesArray.filter((campsite) => favorites.includes(campsite.id))}
+                renderItem={renderFavoriteItem}
+                keyExtractor={(item) => item.id.toString()}
+            />
+        </Animatable.View>
     )
 };
 
